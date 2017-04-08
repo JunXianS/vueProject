40b1bd0a7e91a8e62b333d1770c3c3fe4e873db4
ivanyb <ivanyb@qq.com>
2017-04-08 21:33:39 +0800

    09vue项目 - 首页组件 - 实现首页轮播图功能（图片数据写死在页面上）.avi

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,72 +1,49 @@
 <template>
 	<div id="tmpl">
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-		首页<br>
-
-		首页<br>
-		首页1<br>
-
-
+	 <!-- 1.0 轮播图 -->
+		 <mt-swipe :auto="2000">
+		  <!-- <mt-swipe-item>1</mt-swipe-item>
+		   -->
+		   <mt-swipe-item v-for="item in list">
+		   	<img :src="item.img">
+		   </mt-swipe-item>
+		</mt-swipe>
 	</div>
 </template>
 
 <script>
 	export default{
-
+		data(){
+			return {
+			list:[
+				    {
+				      "url": "http://www.itcast.cn/subject/phoneweb/index.html",
+				      "img": "http://8867345.s21i-8.faiusr.com/2/ABUIABACGAAg_dj2wwUowvbjywUwgAU4wAI!640x640.jpg"
+				    },
+				    {
+				      "url": "http://www.itcast.cn/subject/phoneweb/index.html",
+				      "img": "http://8867345.s21i-8.faiusr.com/2/ABUIABACGAAgmMv3wwUopMqcATCABTjAAg!640x640.jpg"
+				    },
+				    {
+				      "url": "http://www.itcast.cn/subject/phoneweb/index.html",
+				      "img": "http://8867345.s21i-8.faiusr.com/2/ABUIABACGAAgyr6AxAUo2IHXtQMwgAU4wAI!640x640.jpg"
+				    }
+				  ]
+				}
+		}
 	}
 </script>
 
 <style scoped>
-	
+	.mint-swipe{
+		height: 300px;
+	}
+	.mint-swipe-item img{
+		width: 100%;
+	}
+	.mint-swipe-item{
+		background-color: red;
+		width: 100%;
+		height: 300px;
+	}
 </style>
